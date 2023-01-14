10366fb05b6984944fc03c6ff5b379512bda804b
unknown <mrwinrock11@gmail.com>
2023-01-15 01:08:58 +0700

    Stack (not finish)

---
+++ Stack/Work3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Work3 {
+    
+    public static void infixToPostfix() {
+        Scanner input = new Scanner(System.in);
+        String str;
+        char x, n[] = new char[10];
+
+        System.out.print("Infix: ");
+        str = input.next();
+
+        for(int i = 0; i < str.length(); i++) {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
